269737671cd0675ca5911b6da5def32745f131bf
Piotr Krasny <krasny.piotr@gmail.com>
2022-05-01 11:31:27 +0200

    SEO to TS

---
+++ gatsby/src/types/sanityQuery.ts
@@ -0,0 +1,24 @@
+export interface SanityQuery {
+    sanityWebsiteSettings: {
+        title?: string,
+        description?: string,
+        lang?: string,
+        svgFavicon?: {
+            _rawAsset?: {
+                metadata?: {
+                    lqip?: string,
+                },
+                url: string,
+            },
+        }
+        sitePreviev?: {
+            _rawAsset?: {
+                metadata?: {
+                    lqip?: string,
+                },
+                url: string,
+            },
+        }
+        keywords?: string,
+    }
+}
