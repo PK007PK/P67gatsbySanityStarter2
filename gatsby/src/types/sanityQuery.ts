b5ce40a75a708d12dbebbe3952aefc1419dc8b5a
Piotr Krasny <krasny.piotr@gmail.com>
2022-05-07 19:22:46 +0200

    Add hook to source formal name from CMS

--- gatsby/src/types/sanityQuery.ts
+++ gatsby/src/types/sanityQuery.ts
@@ -20,6 +20,7 @@ export interface SanityQuery {
             },
         }
         keywords?: string,
+        websiteOwnerFormalName: string,
         copyright: string,
     },
     sanityMenuData: {
