350d4a2dae47328a4794a3a29b71706ef7202363
Piotr Krasny <krasny.piotr@gmail.com>
2022-05-08 11:34:44 +0200

    Upgrade component

---
+++ gatsby/src/components/HeroTextBlock/HeroTextBlock.style.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import { StylingProps } from 'types/stylingProps';
+
+interface Props extends StylingProps {}
+
+export const HeroTextBlockStyle = styled.div<Props>``;
