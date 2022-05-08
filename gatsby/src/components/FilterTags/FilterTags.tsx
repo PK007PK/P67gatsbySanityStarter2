ed165a2882b0fc5f0ef0c91ce70d704c0cab0e3a
Piotr Krasny <krasny.piotr@gmail.com>
2022-05-08 11:13:02 +0200

    Upgrtade component to TS and use custom hook

---
+++ gatsby/src/components/FilterTags/FilterTags.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from 'react';
+import { Link } from 'gatsby';
+import { pathCheck } from 'utils/pathCheck';
+import { tagsUsed } from '../../hooks/tagsUsed';
+import { FilterTagsStyle } from './FilterTags.style';
+import { useFilterTagsGraphQLData } from './useFilterTagsGraphQLData'
+
+export const FilterTags: FC<{location: Location}> = ({ location }) => {
+    const counts = tagsUsed();
+    const tags = useFilterTagsGraphQLData();
+
+    return (
+        <FilterTagsStyle>
+            {tags.map((tag) => {
+                if (counts[tag.name] > 0) {
+                    return (
+                        <Link
+                            to={`/${tag.slug.current}/1#blog`}
+                            style={pathCheck(location, tag.slug.current) ? { color: 'var(--colorActiveSecondary)' } : null}
+                            key={tag.slug.current}
+                        >
+                            {tag.name} ({counts[tag.name]})
+                        </Link>
+                    );
+                }
+            })}
+        </FilterTagsStyle>
+    );
+}
