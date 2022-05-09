85542eefe259d889f43c8eb4399f01043cf12697
Piotr Krasny <krasny.piotr@gmail.com>
2022-05-09 12:15:21 +0200

    Translate component

---
+++ gatsby/src/components/OptionalBlogPostComponents/OptionalBlogPostComponents.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { OptionalBlogPostComponentsStyle } from './OptionalBlogPostComponents.style';
+
+import CardContactForm from 'components/molecules/CardContactForm/CardContactForm';
+import { Newsletter } from 'components/organisms/Newsletter/Newsletter';
+import { SocialShare } from 'components/molecules/SocialShare/SocialShare';
+
+interface Props {
+    data: number[],
+    name: string,
+    excerpt: string,
+}
+
+export const OptionalBlogPostComponents: React.FunctionComponent<Props> = ({ data, name, excerpt }) => {
+    console.log(data);
+    
+    return (
+        <OptionalBlogPostComponentsStyle>
+            {data.map((item, i) => {
+                switch (item) {
+                    case 0:
+                        return <CardContactForm key={i} className="item" />;
+                    case 1:
+                        return <SocialShare key={i} className="item" title={name} excerpt={excerpt} messengerID={"1234"} />;
+                    case 2:
+                        return <Newsletter key={i} className="item" />;
+                    default:
+                        break;
+                }
+            })}
+        </OptionalBlogPostComponentsStyle>
+    )
+};
+
