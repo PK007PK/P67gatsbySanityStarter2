0b311f243e1a4003b53dfa58e74bf99de8016990
Piotr Krasny <krasny.piotr@gmail.com>
2022-06-18 15:54:01 +0200

    Fix

--- gatsby/gatsby-browser.js
+++ gatsby/gatsby-browser.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './src/styles/index.css';
 import 'normalize.css';
 
-import { AppProvider } from './src/AppProvider';
+import { AppProvider } from './src/AppProvider.tsx';
 
 export function wrapRootElement({ element }) {
     return <AppProvider>{element}</AppProvider>;
